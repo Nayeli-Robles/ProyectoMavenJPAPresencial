ca4519d565772168e046d40e19f83867b1872811
Nayeli-Robles <NayeRobles@gmail.com>
2024-10-01 20:57:45 -0500

    c

--- src/main/java/daoImp/TblProductoImp.java
+++ src/main/java/daoImp/TblProductoImp.java
@@ -1,5 +1,40 @@
 package daoImp;
 
-public class TblProductoImp {
+import java.util.List;
+
+import interfaces.IProducto;
+import model.TblProducto;
+
+public class TblProductoImp implements IProducto {
+
+	@Override
+	public void RegistrarProducto(TblProducto pro) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ActualizarProducto(TblProducto pro) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void EliminarProducto(TblProducto pro) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<TblProducto> ListarProducto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TblProducto BuscarProducto(TblProducto pro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/interfaces/Icliente.java
+++ src/main/java/interfaces/Icliente.java
@@ -1,5 +1,11 @@
 package interfaces;
 
-public interface Icliente {
+import java.util.List;
 
+import model.TblCliente;
+import model.TblProducto;
+
+public interface Icliente {
+	void RegistrarCliente(TblCliente cli);
+	;
 }
