6ea00b914811889f96094256e8797338e0a47724
Nayeli-Robles <NayeRobles@gmail.com>
2024-10-01 22:24:42 -0500

    JPA

---
+++ src/main/java/TestEntiades/TestProducto.java
@@ -0,0 +1,54 @@
+package TestEntiades;
+
+
+
+import java.util.Date;
+
+import daoImp.TblProductoImp;
+import model.TblProducto;
+
+public class TestProducto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TblProducto p=new TblProducto();
+		TblProductoImp dao= new TblProductoImp();
+		
+		p.setNomprod("Arroz INTegrall");
+		p.setPrecio(3.5);
+		p.setCantidad(4);
+		p.setTotal(p.getPrecio()*p.getCantidad());
+		p.setCodbarras("77789");
+		p.setNrolote("lote688");
+		Date fecha=new Date();
+		Date fechasql=new Date(fecha.getTime());
+		dao.RegistrarProducto(p);
+		System.out.println("Se registro correctamente");
+		
+	/*	//**********ACTUALIZAR*************************
+		p.setIdproducto(1);
+		p.setNomprod("");
+		p.setPrecio(1);
+		p.setCantidad(2);
+		p.setTotal(2);
+		p.setCodbarras("");
+		p.setNrolote("");
+		p.setFechaven(null);
+		dao.ActualizarProducto(p);
+		System.out.println("Se actualizo correctamente");
+		
+		
+		//**********ACTUALIZAR*************************
+				p.setIdproducto(1);
+				p.setNomprod("");
+				p.setPrecio(1);
+				p.setCantidad(2);
+				p.setTotal(2);
+				p.setCodbarras("");
+				p.setNrolote("");
+				p.setFechaven(null);
+				dao.ActualizarProducto(p);
+				System.out.println("Se actualizo correctamente");*/
+	}
+
+}
--- src/main/java/daoImp/TblProductoImp.java
+++ src/main/java/daoImp/TblProductoImp.java
@@ -2,6 +2,10 @@ package daoImp;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import interfaces.IProducto;
 import model.TblProducto;
 
@@ -9,32 +13,87 @@ public class TblProductoImp implements IProducto {
 
 	@Override
 	public void RegistrarProducto(TblProducto pro) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("ProyectoJPAMavenMartesNoche");
+		EntityManager em=emf.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.persist(pro);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			e.getMessage();
+		}finally {
+			em.close();
+		}
 		
 	}
 
 	@Override
 	public void ActualizarProducto(TblProducto pro) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("ProyectoJPAMavenMartesNoche");
+		EntityManager em=emf.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.merge(pro);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			e.getMessage();
+		}finally {
+			em.close();
+		}
 		
 	}
 
 	@Override
 	public void EliminarProducto(TblProducto pro) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("ProyectoJPAMavenMartesNoche");
+		EntityManager em=emf.createEntityManager();
+		try {
+			em.getTransaction().begin();
+			TblProducto elipro=em.find(TblProducto.class, pro.getIdproducto());
+			if(elipro!=null){
+				em.remove(elipro);
+				em.getTransaction().commit();
+			}
+		} catch (Exception e) {
+			if(em.getTransaction().isActive()) em.getTransaction().rollback();
+		}finally {
+			em.close();
+		}
 		
 	}
 
 	@Override
 	public List<TblProducto> ListarProducto() {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("ProyectoJPAMavenMartesNoche");
+		EntityManager em=emf.createEntityManager();
+		List<TblProducto> listado=null;
+		try {
+			em.getTransaction().begin();
+			listado=em.createQuery("select c from TblProducto c", TblProducto.class).getResultList();
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			e.getMessage();
+		}finally {
+			em.close();
+		}
+		return listado;
 	}
 
 	@Override
 	public TblProducto BuscarProducto(TblProducto pro) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("ProyectoJPAMavenMartesNoche");
+		EntityManager em=emf.createEntityManager();
+		TblProducto produ=null;
+		try {
+			em.getTransaction().begin();
+			produ=em.find(TblProducto.class, pro.getIdproducto());
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			e.getMessage();
+		}finally {
+			em.close();
+		}
+		return produ;
 	}
 
 }
